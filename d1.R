c6c78bca8039f5cdb440e64ed94b49a7f12fa29c
Lab4-18 <epsTri>
2022-09-22 18:25:00 +0700

    up

---
+++ d1.R
@@ -0,0 +1,15 @@
+#hàm C() dùng để tạo 1 vector
+x <- c(12,54,13,10,5)
+
+#R Căn băn
+# 1.In thông báo ra màn hình 
+print("Hello")
+
+#2.Khai báo biến
+x = 13.8
+print(x)
+
+#3. Kiểm tra kiểu dữ liệu của biến số
+a = TRUE
+print(a)
+print(class(a))
